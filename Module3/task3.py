21e7e986c08d1d51b91cb5412b30719c9b88cdb3
Assignments <mr.radin.bagheri@gmail.com>
2025-09-04 11:22:47 +0330

    I have completed module 3 task 3 again

--- Module3/task3.py
+++ Module3/task3.py
@@ -3,16 +3,16 @@ user = float(input("enter your hemoglobin value(g/l): "))
 if user_gender == "male":
     if user < 134:
         print("your hemoglobin value is low")
-    if user > 167:
+    elif user > 167:
         print("your hemoglobin value is high")
-    if 134 <= user <= 167:
+    else:
       print("your hemoglobin value is normal")
-if user_gender == "female":
+elif user_gender == "female":
     if user  < 117:
         print("your hemoglobin value is low")
-    if user > 155:
+    elif user > 155:
         print("your hemoglobin value is high")
-    if 117 <= user <= 155:
+    else:
         print("your hemoglobin value is normal")
 
 
